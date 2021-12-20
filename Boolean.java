92812bb9b3f339e441bd0408b96acc0c40bdd63b
NaveengrXworkz <naveengr.xworkz@gmail.com>
2021-12-20 16:22:11 +0530

    programe for boolean data type

---
+++ Boolean.java
@@ -0,0 +1,15 @@
+class Boolean{
+     static boolean []truthorfalse = {5<4,7>1,6>4,5>2,6<6,5>4,5>7,4>3,5<2,8<9};
+     public static void main(String[] args){
+     System.out.println(truthorfalse[0]);
+     System.out.println(truthorfalse[1]);
+     System.out.println(truthorfalse[2]);
+     System.out.println(truthorfalse[3]);
+     System.out.println(truthorfalse[4]);
+     System.out.println(truthorfalse[5]);
+     System.out.println(truthorfalse[6]);
+     System.out.println(truthorfalse[7]);
+     System.out.println(truthorfalse[8]);
+     System.out.println(truthorfalse[9]);
+     }
+}
